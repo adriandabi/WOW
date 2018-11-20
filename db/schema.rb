e970780c522ff0b49bee6574ca856475618c8421
adrian dabi <adrian.dabi@hotmail.com>
2018-11-20 01:18:54 -0800

    migration of add-color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_040437) do
+ActiveRecord::Schema.define(version: 2018_11_20_004336) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_18_040437) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
