728103d550695809cb6c6bf3b9f9dfe4c7a557f0
adrian dabi <adrian.dabi@hotmail.com>
2018-11-24 00:59:16 -0800

    orders rout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'simple_pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
