7044193fba4083122cd5a2626cb59d160542f752
adrian dabi <adrian.dabi@hotmail.com>
2018-11-24 12:42:24 -0800

    orders rout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_004336) do
+ActiveRecord::Schema.define(version: 2018_11_24_110708) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
