49ed79807f5b3ad92821cd59517410e8e65b7b1e
adrian dabi <adrian.dabi@hotmail.com>
2018-11-27 13:04:09 -0800

    4.6 orders kinda works but has bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_110708) do
+ActiveRecord::Schema.define(version: 2018_11_27_205540) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_24_110708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.float "price"
   end
 
 end
