dd9fcacdce318f79ff239fc1ff0d93e113150e82
adrian dabi <adrian.dabi@hotmail.com>
2018-12-07 19:29:00 -0800

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_205540) do
+ActiveRecord::Schema.define(version: 2018_12_07_230624) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2018_11_27_205540) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.float "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
