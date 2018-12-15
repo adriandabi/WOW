535965f20e5c98df72b4f5d129fe36d2ff5d66e4
adrian dabi <adrian.dabi@hotmail.com>
2018-12-14 22:48:50 -0800

    5.8comment model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, :set_user, only: [:show, :edit, :update, :destroy]
-  load_and_authorize_resource
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,17 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
 
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
 
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 
 end
