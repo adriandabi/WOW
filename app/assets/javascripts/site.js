9676133141e2699a75f0ef03a0ae63a79cb298f0
adrian dabi <adrian.dabi@hotmail.com>
2019-03-02 12:33:30 -0800

    6.10 AJAX?

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,17 +1,28 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty({
-      starHalf: '<%= asset_path 'star-half.png' %>',
-      starOff: '<%= asset_path 'star-off.png' %>',
-      starOn: '<%= asset_path 'star-on.png' %>',
-      scoreName: 'comment[rating]'
-    });
+var refreshRating = function() {
+	  $('.rating').raty({
+	    starHalf: '<%= asset_path 'star-half.png' %>',
+	    starOff: '<%= asset_path 'star-off.png' %>',
+	    starOn: '<%= asset_path 'star-on.png' %>',
+	    scoreName: 'comment[rating]'
+	  });
     $('.rated').raty({
-     starHalf: '<%= asset_path 'star-half.png' %>',
-     starOff: '<%= asset_path 'star-off.png' %>',
-     starOn: '<%= asset_path 'star-on.png' %>',
-     readOnly: true,
-     score: function() {
-      return $(this).attr('data-score');
-      }
-    });
-});
+  	    starHalf: '<%= asset_path 'star-half.png' %>',
+  	    starOff: '<%= asset_path 'star-off.png' %>',
+  	    starOn: '<%= asset_path 'star-on.png' %>',
+      readOnly: true,
+      score: function() {
+  	      return $(this).attr('data-score');
+  	    }
+  	  });
+    };
+
+    	$(document).on('turbolinks:load', function() {
+    	    refreshRating();
+
+    	    $('.img-zoom').elevateZoom({
+    	      zoomWindowFadeIn: 500,
+    	      zoomWindowFadeOut: 500,
+    	      lensFadeIn: 500,
+    	      lensFadeOut: 500
+    	    });
+      });
