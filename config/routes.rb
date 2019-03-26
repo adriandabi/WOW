5befafd072601b69d27dd6c3058007e7353186dd
adrian dabi <adrian.dabi@hotmail.com>
2019-03-26 12:55:03 -0700

    6.13 maybe correct

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'simple_pages/thank_you'
 
   get 'simple_pages/index'
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
